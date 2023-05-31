4590c44f500aa874efcb3eb3d721d99cb0fa8ecc
svartse <killbillbeatrixx@gmail.com>
2023-06-01 01:16:50 +0300

    made api request

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,1 +1,13 @@
-const key = 'XjCkNlsYLfFriNDi0cN4PA6keyWDWMwH';
+const key = '139k6uJE8vUf1DLWhGGRKWlcRv0EIfEF';
+
+const getCity = async(city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search' //This is the basic URL of the API endpoint that we want to make a request to.
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    console.log(data);
+}
+
+getCity('london');
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const fs = require('fs');
+console.log("rfrgergerge");
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -11,13 +11,16 @@ app.get('/styles.css', (req, res) => {
   res.sendFile(__dirname + '/styles.css');
 });
 
-/* app.get('/app.js', (req, res) => {
-  res.sendFile(__dirname + '/app.js');
-}); */
-
-app.get('/js/:filename', (req, res) => {
+app.get('/scripts/:filename', (req, res) => {
   const { filename } = req.params;
-  const filePath = path.join(__dirname, 'scripts', filename);
+  const filePath = path.join(__dirname, './scripts', filename);
+
+  res.sendFile(filePath);
+});
+
+/* app.get('/js/:filename', (req, res) => {
+  const { filename } = req.params;
+  const filePath = path.join(__dirname, './scripts' + filename);
 
   fs.readFile(filePath, (err, data) => {
     if (err) {
@@ -27,7 +30,7 @@ app.get('/js/:filename', (req, res) => {
       res.type('text/javascript').send(data);
     }
   });
-});
+}); */
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
